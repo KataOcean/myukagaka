e86187989ba22d6c376bfdbc9f2b1691d1d8ae2c
Kata <kataocean@gmail.com>
2017-09-12 12:25:46 +0900

    外部スクリプトの更新など

---
+++ character/ちや/js/freeTalk.js
@@ -0,0 +1,22 @@
+exports = function(callback) {
+
+    var rep = "";
+    fs.readFile(__characterDir + "/serifs/freeTalk.json", 'utf8', (err, data) => {
+        if (err) return;
+        var json = JSON.parse(data);
+        var ary;
+        if (arg) {
+            ary = json[arg[0]];
+        }
+        if (!ary) {
+            ary = json["適当返答"];
+        }
+
+        rep = ary[Math.floor(Math.floor(Math.random() * ary.length))];
+
+        rep = rep.replace(/<呼び名>/, user.nickname);
+
+        callback(rep);
+    });
+
+}
