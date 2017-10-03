b15f57483fe29808b970f4fe802a4668c79e641b
kata <kataocean@gmail.com>
2017-10-04 01:24:43 +0900

    try-catchで例外を受け取るように

--- character/ちや/scripts/commitSelf.js
+++ character/ちや/scripts/commitSelf.js
@@ -15,24 +15,24 @@ exports = function(arg, callback) {
     var addAndCommit = function() {
         try {
 
-            throw "aaa";
-            return;
+            execSync("git add -A");
+            execSync("git config --global user.name " + name + "@ちや");
+            execSync("git commit -m " + arg[0]);
+            exec("git push origin " + branch, (err, stdout, stderr) => {
+                if (err) { console.log(err); }
+                execSync("git config --global user.name " + name);
+                common.getSerifs(__characterDir, "commitSelf", function(serifs) {
+                    rep = common.getRandomSerif(serifs["end"]);
+                    callback({ serif: rep });
+                });
+            });
+
         } catch (ex) {
             rep = "あら？エラーが出たみたいよ？\n" + ex + "\nですって。";
-            callback(rep);
+            callback({ serif: rep });
         }
 
-        execSync("git add -A");
-        execSync("git config --global user.name " + name + "@ちや");
-        execSync("git commit -m " + arg[0]);
-        exec("git push origin " + branch, (err, stdout, stderr) => {
-            if (err) { console.log(err); }
-            execSync("git config --global user.name " + name);
-            common.getSerifs(__characterDir, "commitSelf", function(serifs) {
-                rep = common.getRandomSerif(serifs["end"]);
-                callback({ serif: rep });
-            });
-        });
+
     }
 
     name = execSync("git config --global user.name");
